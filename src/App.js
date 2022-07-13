c64f945b2c78a069e6862a5477df3d6bad7cb468
sushil <sushilbala763@gmail.com>
2022-07-13 14:23:16 -0400

    add all files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
+import AddEmployee from './components/AddEmployee';
+import EmployeeList from './components/EmployeeList';
+import Navbar from './components/Navbar';
+import UpdateEmployee from './components/UpdateEmployee';
 
 function App() {
   return (
 
-    <div className="App">
-      
-    </div>
+    <>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+    <Route index element={<EmployeeList />}></Route>
+    <Route path="/" element={<EmployeeList />}></Route>
+    <Route path="/employeeList" element={<EmployeeList />}></Route>
+    <Route path="/addEmployee" element={<AddEmployee />}></Route>
+    <Route path="/editEmployee/:id" element={<UpdateEmployee />}></Route>
+    </Routes>
+    </BrowserRouter>
+    </>
+
+
+
   );
 }
 
